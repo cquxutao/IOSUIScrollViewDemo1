b166a604c441fd346653ab432bcd2351b0da2e51
cquxutao <xutao1992319@hotmail.com>
2016-04-24 23:53:14 +0800

    Initial Commit

---
+++ ScrollViewDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewDemo1
+//
+//  Created by 徐涛 on 4/24/16.
+//  Copyright © 2016 徐涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewDemo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollViewDemo1
+//
+//  Created by 徐涛 on 4/24/16.
+//  Copyright © 2016 徐涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
